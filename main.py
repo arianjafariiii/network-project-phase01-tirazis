1d85066b47850b341530b213b84b7faa6dbbc638
ArianJafari <arian.jafari2233@gmail.com>
2023-12-03 00:48:20 +0330

    second func update 2
    add comments and clean  the code

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import socket
 
+#Get entries and seperate them to use them in function
 host = input("Enter host : ")
-ports_s = input("Enter ports")
-
+ports_s = input("Enter ports : ")
 ports_l = ports_s.split(' ')
 ports = [int(e) for e in ports_l]
 
 
 
 def check_port(host, port):
+        
+    #Make an object of socket to cannect
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(1)
 
+#Check is the host online or not and if yes get other information
     try:
         sock.connect((host, port))
-        print(f"Port {port} is open")
+        #Get service of the port
         service_name = socket.getservbyport(port, 'tcp')
-        print(service_name)
+        print(f"{host} is online      --port: {port}     --service: {service_name}"  )
+        
     except socket.error:
-        print(f"Port {port} is closed")
+        print(f"{host} is offline")
+        return True
     finally:
         sock.close()
 
